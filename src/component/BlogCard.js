88606421ce2b6365df49a13f38b5205bf18a9f42
omkar <omkar13021@gmail.com>
2023-09-04 11:56:40 +0000

    updated blog card

--- src/component/BlogCard.js
+++ src/component/BlogCard.js
@@ -2,24 +2,12 @@ import React from 'react'
 
 const BlogCard = (props) => {
     return (
-        <div className='blog-post'>
+        <div className='blog-post' >
             <article className='blog-article' key={props.blog.id}>
                 <h2>{props.blog.title}</h2>
-                <p className='blog-content'>{props.blog.content}
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                    vestibulum sagittis turpis at bibendum. Nunc nec bibendum quam.
-                    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-                    posuere cubilia curae; Sed quis tellus at quam faucibus varius.
-                    Vivamus rhoncus arcu id neque faucibus, id dapibus nulla dictum.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem officia commodi similique
-                    totam nemo voluptatibus nihil velit doloremque repudiandae, consequuntur voluptate mollitia,
-                    facere corporis autem? Magnam vero voluptatem minima laborum. Nesciunt optio ratione deserunt
-                    odit labore! Natus porro exercitationem nisi deleniti suscipit earum aperiam cupiditate! Tempore
-                    nobis quae, natus, temporibus nisi sequi, et illo culpa consectetur iure nemo dolorem. Quia quod
-                    non sint nobis debitis.
-                </p>
+                <p className='blog-content overflow-hidden'style={{height: "200px"}}>{props.blog.content}</p>
                 <button className='blog-button'>view</button>
-                <span className='post-date'>Posted on: August 15, 2023 by {props.blog.author}</span>
+                <span className='post-date'>Posted on:{props.blog.updatedAt} by {props.blog.author}</span>
             </article>
         </div>
     )
